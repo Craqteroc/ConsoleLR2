c7a84a243f3749c1dd98dade5508ae4ed03a6d6a
Craqteroc <vlad_flyfly@mail.ru>
2025-02-03 18:33:13 +0500

    FixClass

---
+++ ConsoleLR2/FixClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleLR2
+{
+    internal class FixClass
+    {
+    }
+}
